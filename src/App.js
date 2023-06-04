a21a378dad329833acf5a3785de6704c8790a78f
Kayc <ghstkc@gmail.com>
2023-06-04 21:13:37 +0545

    Added home page with buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WebCam from './WebCam';
+import './App.css';
 
 const App = () => {
+  const [isCapturePhoto, setIsCapturePhoto] = useState(false);
+  
+  const handleCaptureClick = () => {
+    setIsCapturePhoto(true);
+  }
+
   return (
-    <WebCam />
+    <div className="main">
+      <h1 className="title">Emotion Detector</h1> // your title here
+      <WebCam isCapturePhoto={isCapturePhoto} setIsCapturePhoto={setIsCapturePhoto} />
+      <div className="buttons">
+        <button className="button" onClick={() => setIsCapturePhoto(false)}>Detect with Video</button>
+        <button className="button" onClick={handleCaptureClick}>Detect with Photo</button>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
